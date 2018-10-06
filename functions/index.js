213c376402013759e30b46de453784d3e4bbd40c
George Gomes <georgegomees@gmail.com>
2018-10-06 12:47:53 -0300

    first version with functions working

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,44 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+var db = admin.firestore();
 
-const firestore = new Firestore();
-const settings = {/* your settings... */ timestampsInSnapshots: true};
-firestore.settings(settings);
+exports.addUser = functions.https.onRequest((req, res) => { 
+    const userCollection = db.collection("user");
+    const userToSave = req.body.data
 
-exports.addAUser = functions.https.onRequest((req, res) => {
-    try {
-        const userCollection = admin.firestore().collection("user");
+    return userCollection.add(userToSave)
+            .then(re => {
+                return res.status(200).send(re._referencePath);
+            }).catch(err => {
+                return res.status(500).send(err);
+            });
+});
 
-        var userToSave = {
-            name: req.body.data.name,
-            idade: req.body.data.idade
-        };
+exports.addRoom = functions.https.onRequest((req, res) => {
 
-        console.log(userToSave);
+    const roomCollection = db.collection("room");
+    const roomToSave = req.body.data
 
-        userCollection.add(userToSave);
-    } catch (error) {
-        console.log('DAAM:', error);
-        return error;
-    }
+    return roomCollection.add(roomToSave)
+            .then(re => {
+                console.log(re._referencePath);
+                return res.status(200).send(re._referencePath);
+            }).catch( err =>{
+                console.log("deu ruim");
+                return res.status(500).send(err);
+        });
+});
+
+exports.getRooms = functions.https.onRequest((req, res) => {
+
+});
+
+exports.functionModel = functions.https.onRequest((req, res) => {
+
+});
+
+exports.testServer = functions.https.onRequest((req, res) =>{
+    res.status(200).send({"test": req.body})
 });
 
