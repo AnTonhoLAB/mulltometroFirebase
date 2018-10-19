3bbc5d846ba61bed217d29a56be5f18874801ff8
George Gomes <georgegomees@gmail.com>
2018-10-18 21:44:24 -0300

    Add room and set rooms in user

--- functions/index.js
+++ functions/index.js
@@ -6,20 +6,23 @@ var db = admin.firestore();
 exports.addUser = functions.https.onRequest((req, res) => { 
     const userCollection = db.collection("user");
     const userToSave = req.body.data
+    console.log("Criando user" + req.body.data.name);
 
-    return userCollection.doc(userToSave.id).set(userToSave)
-            .then(re => {
-                return res.status(200).send(re._referencePath);
-            }).catch(err => {
-                return res.status(500).send(err);
-            });
+    return userCollection.doc(userToSave.uid).set(userToSave)
+        .then(re => {
+            return res.status(200).send(re);
+        })
+        .catch(err => {
+            return res.status(500).send(err);
+        });
 });
 
 exports.addRoom = functions.https.onRequest((req, res) => {
     const userCollection = db.collection("user");
     const roomCollection = db.collection("room");
-    const roomToSave = req.body.data
+    const roomToSave = req.body.data;
 
+    var roomToReturn = [];
 
     return roomCollection.add(roomToSave)
             .then( roomSaved => {
@@ -30,20 +33,20 @@ exports.addRoom = functions.https.onRequest((req, res) => {
                             var adminRoomArray = snapshot.data().adminRooms
 
                             if  (adminRoomArray && adminRoomArray.length) {
-                                adminRoomArray.push('newfield');
+                                adminRoomArray.push(roomSaved.id);
                             } else {
-                                adminRoomArray = ["oldField"];
+                                adminRoomArray = [roomSaved.id];
                             }
-                            console.log("setup array " + adminRoomArray);
-
+                            roomToReturn = snapshot.data();
+                        
                             return transaction.update(db.collection("user").doc(roomToSave.adminId), { adminRooms: adminRoomArray } );
                         });
                     });
                 })
             .then( reess => {
-                console.log("save transaction");
+                console.log("save transaction with: " + reess);
                 
-                return res.status(200).send({ data: roomToSave} );
+                return res.status(200).send(reess);
             })
             .catch( err =>{
                 console.log("deu ruim: " + err );
@@ -51,8 +54,64 @@ exports.addRoom = functions.https.onRequest((req, res) => {
             })
 });
 
-exports.getRooms = functions.https.onRequest((req, res) => {
+exports.addRoomCustom = functions.https.onRequest((req, res) => {
 
+    const roomCollection = db.collection("room");
+    const userCollection = db.collection("user");
+
+    const roomToSave = req.body.data;
+
+    var roomToReturn = [];
+
+    return roomCollection.add(roomToSave)
+        .then( roomSaved => {
+
+            var user = userCollection.doc(roomToSave.adminId)
+
+            var adminRoomArray = userCollection.doc(roomToSave.adminId).get("adminRooms");
+            
+            console.log("OPA" + typeof adminRoomArray);
+
+            if  (adminRoomArray.length > 0) {
+                adminRoomArray.push(roomSaved);
+            } else { 
+                adminRoomArray = [roomSaved]
+            }
+            console.log("array: " + adminRoomArray);
+            
+            userCollection.doc(roomToSave.adminId).update({ "adminRooms": adminRoomArray});
+
+
+            // console.log("user: " + user);
+            
+
+            // userCollection.doc(roomToSave.adminId).set(user);
+
+
+
+            // return db.collection("user").doc(roomToSave.adminId).onSnapshot( snp => {
+            //     var data = snp.data;
+            //     // var admRooms = snp.data().adminRooms;
+            //     data.admRooms.push(roomSaved);          
+            //     console.log("1");
+                
+            //     db.collection("user").doc(roomToSave.adminId).update(snp.data());
+            //     console.log("2");
+            //     return res.status(200).send(snp.data().adminRooms);
+            // })
+
+            
+        //   return  res.status(200).send( db.collection("user").doc(roomToSave.adminId)) ;
+
+                // var adminRoomArray = snapshot.data().adminRooms
+            return  res.status(200).send(userCollection.doc(roomToSave.adminId).get("adminRooms"));
+
+        })
+    .catch( err => {
+        console.log(err);
+        
+        return res.status(500).send(err);
+    });
 });
 
 exports.functionModel = functions.https.onRequest((req, res) => {
